8c1ebddc2d9ca20c3241ff6977807547d5a2442a
NicoliCo <nicoli@gmail.com>
2017-12-11 00:16:32 -0700

    V0

---
+++ run_analysis.R
@@ -0,0 +1,74 @@
+
+# load activity names
+activities=read.table("UCI HAR Dataset/activity_labels.txt",
+                           col.names = c("id","activity"),header = FALSE)
+
+# load feature names
+features=read.table("UCI HAR Dataset/features.txt",
+                    col.names = c("id","feature"),header = FALSE,
+                    colClasses = c("numeric","character"))
+features = features$feature
+# save columns that are mean() or std()
+maskMeanStd = (grepl("mean()",features,fixed = TRUE)|
+                   grepl("std()",features,fixed=TRUE))
+
+# make feature names easier to read
+features = gsub("[()]","",features)
+features = gsub("Acc","Accelerometer",features)
+features = gsub("Gyro","Gyroscope",features)
+features = gsub("mean-([XYZ])","\\1-mean",features)
+features = gsub("mean","Mean",features)
+features = gsub("std-([XYZ])","\\1-std",features)
+features = gsub("std","Std",features)
+features = gsub("^t","time",features)
+features = gsub("^f","frequency",features)
+features = gsub("-","",features)
+features = gsub("Mag","Magnitude",features)
+
+
+
+#### load training set
+train=read.table("UCI HAR Dataset/train/X_train.txt",
+                    colClasses=rep("numeric",561),header = FALSE,
+                    col.names = features, row.names = NULL)
+trainSubjectIDs = read.table("UCI HAR Dataset/train/subject_train.txt", header = FALSE)
+trainActivityLabels = read.table("UCI HAR Dataset/train/y_train.txt", header = FALSE)
+
+#### load test set
+test=read.table("UCI HAR Dataset/test/X_test.txt",
+                colClasses=rep("numeric",561),header = FALSE,
+                col.names = features, row.names = NULL)
+testSubjectIDs = read.table("UCI HAR Dataset/test/subject_test.txt", header = FALSE,
+                            colClasses = "character")
+testActivityLabels = read.table("UCI HAR Dataset/test/y_test.txt", header = FALSE)
+
+
+## merge activity labels
+tmp = merge(testActivityLabels,activities,by.y="id",by.x="V1")
+test$activity = tmp$activity
+tmp = merge(trainActivityLabels,activities,by.y="id",by.x="V1")
+train$activity = tmp$activity
+
+## integrate subject IDs
+test$subjectID = as.factor(testSubjectIDs$V1)
+train$subjectID = as.factor(trainSubjectIDs$V1)
+
+## combine training and test
+alldata = rbind(train,test)
+
+# keep only mean and std cols
+alldata=alldata[,maskMeanStd]
+
+# make tidy data
+s=split(alldata,alldata$activity)
+activity=sapply(s,function(x) colMeans(x[1:66]))
+
+s=split(alldata,alldata$subjectID)
+subject=sapply(s,function(x) colMeans(x[1:66]))
+subject=subject[,order(as.numeric(colnames(subject)))]
+
+colnames(subject)=gsub("([0-9]+)","Subject\\1",colnames(subject))
+
+tidydata=cbind(activity,subject)
+
+tidydata
